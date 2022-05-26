7919824c45705c6b2f294e6cac6822fcadcfb7e3
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 05:04:07 +0200

    update src/helpers/index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,11 +6,13 @@ import {
 import { generatePassword } from './_password.helper';
 import sendEmail from './_sendEmail.helper';
 import notifications from './_notifications.helper';
+import validationSchema from './_validationSchema.helper';
 
 export {
   generateToken,
   decodeJWT,
   generatePassword,
   sendEmail,
-  notifications
+  notifications,
+  validationSchema
 };
