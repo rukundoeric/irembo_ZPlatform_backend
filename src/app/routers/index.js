79e2319b9c4a8620dff841abaa3017fd32475347
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 05:03:21 +0200

    update src/app/routers/index.js

--- src/app/routers/index.js
+++ src/app/routers/index.js
@@ -1,9 +1,11 @@
 import 'regenerator-runtime';
 import express from 'express';
 import auth from './_auth';
+import user from './_user';
 
 const api = express();
 api.use('/auth', auth);
+api.use('/user', user);
 api.get('/', (req, res) => {
   res.json({
     message: 'Welcome to ZPlatform.',
