55ff4cd748d8e94ebef9404b0cf038f3aa0ae0c8
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 11:01:15 +0200

    update accountconfirmationrequest.js

--- src/db/models/accountconfirmationrequest.js
+++ src/db/models/accountconfirmationrequest.js
@@ -1,22 +1,25 @@
-'use strict';
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class AccountConfirmationRequest extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.User, {
+        as: 'account',
+        foreignKey: 'account_id',
+        targetKey: 'user_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      });
     }
   }
   AccountConfirmationRequest.init({
-    request_id: DataTypes.STRING,
+    request_id: {
+      primaryKey: true,
+      type: DataTypes.STRING,
+    },
     account_id: DataTypes.STRING,
-    user_id: DataTypes.STRING,
     status: DataTypes.STRING
   }, {
     sequelize,
