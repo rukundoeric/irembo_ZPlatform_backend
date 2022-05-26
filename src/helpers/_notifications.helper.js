ca0f3672b9b76e3cf78d1b99b2455aa6ad912513
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 12:08:26 +0200

    update _notifications.helper.js

--- src/helpers/_notifications.helper.js
+++ src/helpers/_notifications.helper.js
@@ -1,11 +1,11 @@
 const notifications = {
   account_v_confirmed: () => ({
-    title: 'Account verfication',
+    title: 'Account verfication confirmed ✅',
     description: 'Your account has been verfied! continue to enjoy our services 😃',
   }),
   account_v_denied: (more) => ({
-    title: 'Account verfication',
-    description: `Your account could not be verfied due to the following reasons\n${more}`,
+    title: 'Account verfication denied 😞',
+    description: `Your account could not be verfied due to the following reasons, \n${more}`,
   })
 };
 
