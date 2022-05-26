a9193cc015b210a7309569d97cfeabab6bb75917
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 07:32:26 +0200

    update _user.js

--- src/app/routers/_user.js
+++ src/app/routers/_user.js
@@ -1,14 +1,17 @@
 import 'regenerator-runtime';
 import express from 'express';
 import UserController from '../controllers/User.controller';
-import { checkUserAlreadyExist, checkUserExist } from '../../middlewares/User';
+import { checkUserAlreadyExist, checkUserExist, verifyPasswordResetToken } from '../../middlewares/User';
 import { verifyEmailVerificationToken } from '../../middlewares/Auth';
 import Validator from '../../middlewares/Validator';
 
 const {
   signup,
   verifyUserEmail,
-  resendEmailVerification
+  resendEmailVerification,
+  requestPasswordReset,
+  applyChangePassword,
+  resendPasswordResetConfirmation,
 } = UserController;
 
 const router = express.Router();
@@ -16,5 +19,10 @@ const router = express.Router();
 router.post('/signup', checkUserAlreadyExist, Validator('signup'), signup);
 router.put('/verify-email/:token', verifyEmailVerificationToken, verifyUserEmail);
 router.post('/resend-email-verification', checkUserExist, resendEmailVerification);
+
+// password reset
+router.post('/request-password-reset', checkUserExist, requestPasswordReset);
+router.post('/reset-password/:token', verifyPasswordResetToken, Validator('resetpass'), applyChangePassword);
+router.post('/resend-password-reset-confirmation', checkUserExist, resendPasswordResetConfirmation);
 
 export default router;
