4c5cfaa1f72caed3e5df49d53899827efa96fe0e
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 05:02:54 +0200

    update Auth.controller.js

--- src/app/controllers/Auth.controller.js
+++ src/app/controllers/Auth.controller.js
@@ -26,16 +26,17 @@ class AuthController {
     if (!result) return res.sendStatus(500);
     res
       .cookie('refresh_token', refresh_token, { ...cookieOptions, maxAge: 24 * 60 * 60 * 1000 })
-      .json(access_token);
+      .json({ access_token });
   }
 
   static async sendOneTimePassword(req, res) {
     const { user: { user_id, email } } = req;
-    const password = generatePassword();
-    const one_time_password = await generateToken({ password }, { type: 'one-time-password' }, '5m');
+    const password = generatePassword(true, null, false);
+    const passwordHash = generatePassword(false, password);
+    const one_time_password = await generateToken({ password: passwordHash }, { type: 'one-time-password' }, '5m');
     const result = await Auth.update({ one_time_password }, { where: { user_id } });
     if (!result) return res.sendStatus(500);
-    sendOneTimePassword({ email, password: one_time_password });
+    sendOneTimePassword({ email, password });
     res.json({
       user: { user_id, email },
       message: `Your one time password is sent to your email: ${email}.`,
@@ -49,7 +50,7 @@ class AuthController {
     if (!auth) return res.sendStatus(401);
     const encodedPassword = auth?.one_time_password;
     decodeJWT(encodedPassword, async (err, decoded) => {
-      if (err) return res.status(403).json({ error: { message: 'Password is expired!' } });
+      if (err) return res.status(403).json({ error: { message: 'Password is invalid or expired!' } });
       const isCorrectPassword = bcrypt.compareSync(password, decoded.password);
       if (!isCorrectPassword) return res.status(401).json({ error: { message: 'Password is incorrect!' } });
       const userData = { user_id, email: auth?.user?.email, role: auth?.user?.role };
@@ -57,10 +58,15 @@ class AuthController {
       const refresh_token = await generateToken(userData, { type: 'refresh-token' }, '1d');
       const result = await Auth.update({ refresh_token }, { where: { user_id } });
       if (!result) return res.sendStatus(500);
+      await Auth.update({ one_time_password: null }, { where: { user_id } });
       res
         .cookie('refresh_token', refresh_token, { ...cookieOptions, maxAge: 24 * 60 * 60 * 1000 })
         .json(access_token);
     });
+  }
+
+  static async refreshToken(req, res) {
+    
   }
 }
 
