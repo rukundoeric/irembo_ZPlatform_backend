9433f186549c50175affd6b1b1299a660f08fc78
rukundoeric <ericrukundo005@gmail.com>
2022-05-29 18:49:50 +0200

    update 20220502053158-user.js

--- src/db/seeders/20220502053158-user.js
+++ src/db/seeders/20220502053158-user.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-unused-vars */
 const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
@@ -8,16 +9,47 @@ const { v4: uuidv4 } = uuid;
 dotenv.config();
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const user_id = uuidv4();
     await queryInterface.bulkInsert(
       'Users',
       [
         {
-          user_id: uuidv4(),
+          user_id,
           email: 'moderator@gmail.com',
           password: bcrypt.hashSync('moderator', 10),
           role: '1.1.0',
           email_verified: true,
           account_verified: 'VERIFIED',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+    await queryInterface.bulkInsert(
+      'Profiles',
+      [
+        {
+          user_id,
+          first_name: 'Mugabo',
+          last_name: 'Steve',
+          age: 30,
+          gender: 'MALE',
+          date_of_birth: '12/12/2019',
+          marital_status: 'SINGLE',
+          nationality: 'Rwandan',
+          n_id: '1223242323232',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+    await queryInterface.bulkInsert(
+      'Auths',
+      [
+        {
+          user_id,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
