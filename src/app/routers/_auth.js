47a9cdf8e0bf3ce320351c71a8b805f38972f9e0
rukundoeric <ericrukundo005@gmail.com>
2022-05-30 18:42:29 +0200

    update _auth.js

--- src/app/routers/_auth.js
+++ src/app/routers/_auth.js
@@ -1,20 +1,27 @@
 import 'regenerator-runtime';
 import express from 'express';
 import AuthController from '../controllers/Auth.controller';
-import { checkCredentials } from '../../middlewares/Auth';
+import { checkCredentials, verifyAccessToken } from '../../middlewares/Auth';
 import { checkUserExist } from '../../middlewares/User';
+import Validator from '../../middlewares/Validator';
 
 const {
   login,
   logout,
   sendOneTimePassword,
   secondFactorAuth,
-  refreshToken
+  refreshToken,
+  turnOnOrOffSecondFactorAuth,
+  loginWithAccessToken,
+  validateLoginToken,
 } = AuthController;
-
 const router = express.Router();
 
 router.post('/login', checkUserExist, checkCredentials, login, sendOneTimePassword);
+router.post('/login-with-token', Validator('loginWithToken'), loginWithAccessToken, sendOneTimePassword);
+router.put('/login-with-token/validate', verifyAccessToken, validateLoginToken);
+router.put('/second-factor-auth', verifyAccessToken, turnOnOrOffSecondFactorAuth);
+router.post('/resend-one-time-password', checkUserExist, login, sendOneTimePassword);
 router.post('/second-factor-auth', secondFactorAuth);
 router.get('/refresh-token', refreshToken);
 router.get('/logout', logout);
