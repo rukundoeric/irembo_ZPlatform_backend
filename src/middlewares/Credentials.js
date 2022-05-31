6790dd8835dbacb45e7303103eec306de91bdeb6
rukundoeric <ericrukundo005@gmail.com>
2022-05-31 11:09:57 +0200

    update Credentials.js

--- src/middlewares/Credentials.js
+++ src/middlewares/Credentials.js
@@ -1,6 +1,7 @@
 import config from '../config';
 
 const { allowedOrigins } = config;
+console.log(allowedOrigins);
 export default (req, res, next) => {
   const { origin } = req.headers;
   if (allowedOrigins.includes(origin)) {
